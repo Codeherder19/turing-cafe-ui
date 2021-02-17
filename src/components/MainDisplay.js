6e94f2e716c93b4ab70073e0be2ec540322b11d8
Brian Forbes <brianandrewf81@gmail.com>
2021-02-17 10:04:41 -0700

    Initial components set up, ready to begin setting up state and passing props to display reservations

---
+++ src/components/MainDisplay.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+// import reservationData from '../test-data/reservation-data'
+
+class MainDisplay extends React.Component {
+  render() {
+    return null
+
+  }
+}
+
+export default MainDisplay;
---
+++ src/components/Reservation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+const Reservation = (props) => {
+  return ()
+}
+
+
+
+
+export default Reservation;

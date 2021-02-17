80af9c97302192d8d45552bfe618a84d740c2ec2
Brian Forbes <brianandrewf81@gmail.com>
2021-02-17 12:11:10 -0700

    Set up inputs with values passing state and begin writing handleChange function

--- src/components/ReservationForm.js
+++ src/components/ReservationForm.js
@@ -3,22 +3,30 @@ import React from 'react';
 class ReservationForm extends React.Component {
   constructor() {
     super()
-    this.state ={
-
+    this.state = {
+      name: '',
+      date: '',
+      time: '',
+      guests: '',
+      isBooked: null,
     }
+  }
+
+  handleChange = event => {
+    
   }
 
   render() {
   return (
     <form className="res-form">
       <label for="name"></label>
-      <input type="text" id="name" name="name" placeholder="Name"/>
+      <input type="text" id="name" name="name" placeholder="Name" value={this.state.name}/>
       <label for="date"></label>
-      <input type="date" id="date" name="date" placeholder="Date"/>
+      <input type="date" id="date" name="date" placeholder="Date" value={this.state.date}/>
       <label for="Time"></label>
-      <input type="Time" id="time" name="time" placeholder="Time"/>
+      <input type="Time" id="time" name="time" placeholder="Time" value={this.state.time}/>
       <label for="guests"></label>
-      <input type="number" id="guests" name="guests" placeholder="Guests" min="1" max="10"/>
+      <input type="number" id="guests" name="guests" placeholder="Guests" min="1" max="10" value={this.state.guests}/>
       <button>Make Reservation</button>
     </form>
   )
